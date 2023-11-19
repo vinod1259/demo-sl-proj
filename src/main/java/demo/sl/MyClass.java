7c38b7c0034b1619270911c3856c290a87d7b5d0
Dr. Hitesh Kumar Sharma <hkshitesh@gmail.com>
2023-11-19 11:49:34 +0530

    div function added

--- src/main/java/demo/sl/MyClass.java
+++ src/main/java/demo/sl/MyClass.java
@@ -13,10 +13,15 @@ public class MyClass {
 	{
 		return a*b;
 	}
+	public int div(int a, int b)
+	{
+		return a/b;
+	}
 	public static void main(String[] args) {		
 		MyClass ob = new MyClass();
 		System.out.println("Sum is : "+ob.sum(20, 10));
 		System.out.println("Difference is : "+ob.sub(20, 10));
 		System.out.println("Multiply is : "+ob.mul(20, 10));
+		System.out.println("Division is : "+ob.div(20, 10));
 	}
 }
