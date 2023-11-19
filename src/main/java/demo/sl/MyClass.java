f604ec8aa91e90c014df205de121d11b209ee39f
Hitesh SHarma <hkshitesh@gmail.com>
2023-11-19 09:45:46 +0530

    mul functionadded

--- src/main/java/demo/sl/MyClass.java
+++ src/main/java/demo/sl/MyClass.java
@@ -9,9 +9,14 @@ public class MyClass {
 	{
 		return a-b;
 	}
+	public int mul(int a, int b)
+	{
+		return a*b;
+	}
 	public static void main(String[] args) {		
 		MyClass ob = new MyClass();
 		System.out.println("Sum is : "+ob.sum(20, 10));
-		System.out.println("Difference is : "+ob.sub(20, 10));	
+		System.out.println("Difference is : "+ob.sub(20, 10));
+		System.out.println("Multiply is : "+ob.mul(20, 10));
 	}
 }
